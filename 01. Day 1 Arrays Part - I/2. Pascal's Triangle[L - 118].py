8482cf461311dfbb06e2b23396db20f8e2769411
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-03 22:57:46 +0530

    Day 1 Completed

---
+++ 01. Day 1 Arrays Part - I/2. Pascal's Triangle[L - 118].py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/pascals-triangle/
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+       
+
+        res=[[1]]
+        for i in range(1,numRows):
+            arr=[0]*(i+1)
+            arr[0]=arr[i]=1
+            preArr=res[-1]
+            for j in range(1,(i//2)+1):
+                arr[j]=arr[i-j]=preArr[j]+preArr[j-1]
+            res.append(arr)
+        return res
+        
