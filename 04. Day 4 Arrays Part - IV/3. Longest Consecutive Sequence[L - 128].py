d726ce41c093be18f13b82a98ec1a3265fda53a9
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-06 23:48:11 +0530

    Day 4 Completed

---
+++ 04. Day 4 Arrays Part - IV/3. Longest Consecutive Sequence[L - 128].py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/longest-consecutive-sequence/
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        output = 0
+
+        for n in nums:
+            if n-1 not in nums:
+                start = n
+                while start in nums:
+                    start+=1
+
+                output = max(output, start-n)
+        return output
+
+      
