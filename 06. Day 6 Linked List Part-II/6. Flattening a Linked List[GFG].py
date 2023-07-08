52d5b5e817f3273caacdddae3629a46869273720
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-09 00:35:49 +0530

    Day 6 Completed

---
+++ 06. Day 6 Linked List Part-II/6. Flattening a Linked List[GFG].py
@@ -0,0 +1,33 @@
+# https://practice.geeksforgeeks.org/problems/flattening-a-linked-list/1
+
+'''
+class Node:
+    def __init__(self, d):
+        self.data=d
+        self.next=None
+        self.bottom=None
+'''        
+
+
+def merge(a, b):
+    if not a:
+        return b
+    if not b:
+        return a
+
+    ans = None
+    if a.data < b.data:
+        ans = a
+        ans.bottom = merge(a.bottom, b)
+    else:
+        ans = b
+        ans.bottom = merge(a, b.bottom)
+
+    return ans
+
+def flatten(root):
+    if not root:
+        return None
+
+    mergedLL = merge(root, flatten(root.next))
+    return mergedLL
