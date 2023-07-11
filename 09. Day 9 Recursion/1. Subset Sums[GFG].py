3e9362d8ea9a5ef9ca6b58affbe02517ddb9d33c
anshawasthi01 <anshawasthi3894@gmail.com>
2023-07-11 23:59:57 +0530

    Day 9 Completed

--- 09. Day 9 Recursion/1. Subset Sums[GFG].py
+++ 09. Day 9 Recursion/1. Subset Sums[GFG].py
@@ -1,2 +1,18 @@
 # https://practice.geeksforgeeks.org/problems/subset-sums2234/1
 
+class Solution:
+    def subsetSums(self, arr, N):
+        
+        # code here
+        
+        lst=[0]
+        self.shivesh(arr,0,lst)
+        return lst
+        
+    def shivesh(self,arr,res,lst):
+        if not arr:
+            return
+        
+        for i in range(len(arr)):
+            lst.append(res+arr[i])
+            self.shivesh(arr[i+1:],res+arr[i],lst)
